b6c838226bfff47ff970b4a9fee402f636129d0a
Shauray <shauray9@gmail.com>
2021-05-09 16:56:00 +0530

    creating patches

--- Dino/model.py
+++ Dino/model.py
@@ -84,6 +84,20 @@ class Block(nn.Module):
         x = x + self.drop_path(self.mlp(self.norm(x)))
         return x
 
+class PatchEmbed(nn.Module):
+    def __init__(self, img_size=224, patch_size=16, channels=3, embed_dim=768):
+        super(PatchEmbed, self).__init__()
+        num_patches = (img_size // patch_size) * (img_size // patch_size)
+        self.img_size = img_size
+        self.patch_size = patch_size
+        self.num_patches = num_patches
+
+        self.proj = nn.Conv2d(channels, embed_dim, kernel_size=patch_size, stride=patch_size)
+
+    def forward(self, x):
+        B, C, H, W = x.shape
+        x = self.proj(x).flatten(2).transpose(1, 2)
+        return x
 
 ## main network
 class ViT(nn.Module):
