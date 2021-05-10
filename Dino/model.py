7aea336e26a9ccb5f70b313edf35a576b5753f2e
Shauray <shauray9@gmail.com>
2021-05-10 22:44:35 +0530

    done with the architecture

--- Dino/model.py
+++ Dino/model.py
@@ -298,5 +298,25 @@ class DINOHead(nn.Module):
         x = self.last_layer(x)
         return x
 
-# remaining: DINOHEAD AND SOME MORE FUNCTIONS TO CALL THE MODEL
+
+def deit_tiny(patch_size=16, **kwargs):
+    model = VisionTransformer(
+        patch_size=patch_size, embed_dim=192, depth=12, num_heads=3, mlp_ratio=4,
+        qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    return model
+
+
+def deit_small(patch_size=16, **kwargs):
+    model = VisionTransformer(
+        patch_size=patch_size, embed_dim=384, depth=12, num_heads=6, mlp_ratio=4,
+        qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    return model
+
+
+def vit_base(patch_size=16, **kwargs):
+    model = VisionTransformer(
+        patch_size=patch_size, embed_dim=768, depth=12, num_heads=12, mlp_ratio=4,
+        qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    return model
+
 
