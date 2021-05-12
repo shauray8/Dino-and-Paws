9a9dd9fb4abae1f75ffe39c62b34b6cda8bf9a78
Shauray <shauray9@gmail.com>
2021-05-13 00:00:09 +0530

    augmantations

--- Dino/utils.py
+++ Dino/utils.py
@@ -53,8 +53,69 @@ class DataAugmantation(object):
             ])
 
         # First global crop
-        
+        self.global_transfo1 = transforms.Compose([
+            transforms.RandomResizedCrop(224, scale=global_crops_scale, interpolation=Image.BICUBIC),
+            flip_and_color_jitter,
+            GaussianBlur(1.0),
+            normalize,
+            ])
 
+        # Second global crop
+        self.global_transfo2 = transforms.Compose([
+            transforms.RandomResizedCrop(224, scale=global_crops_scale, interpolation=Image.BICUBIC),
+            flib_and_Color_jitter,
+            GaussianBlur(.1),
+            Solarization(.2),
+            normalizw,
+            ])
+
+        # Transformation for the local small crops
+        self.local_crops_number = local_crops_number
+        self.local_transfo = transforms.Compose([
+            transforms.RandomResizedCrop(96, scale-local_crops_scale, interpolation=Image.BICUBIC),
+            flip_and_color_jitter,
+            GaussianBlur(p=.5),
+            normalize,
+            ])
+
+    def __call__(self, image):
+        crops = []
+        crops.append(self.global_tranfo1(image))
+        crops.append(self.global_transfo2(image))
+        for _ in range(self.local_crops_number):
+            crops.append(self.local_transfo(image))
+
+        return crops
+
+
+## ------------ Gausian Blur ------------ ##
+class GaussianBlur(object):
+    def __init__(self, p=.5, radius_min=.1, radius_max=2.):
+        self.prob=p
+        self.radius_min = radius_min
+        self.radius_max = radius_max
+
+    def __call__(self, img):
+        do_it = random.random() <= self.prob
+        if not do_it:
+            return img
+
+        return img.filter(
+                ImageFilter.GaussianBlur(
+                    radius = random.unifor(self.radius_min, self.radius_max)
+                    )
+                )
+
+## ------------ Solarization ------------ ##
+class Solarization(object):
+    def __init__(self, p):
+        self.p = p
+
+    def __call__(self, img):
+        if random.random() < self.p:
+            return ImageOps.solarize(img)
+        else:
+            return img
 
 
 
